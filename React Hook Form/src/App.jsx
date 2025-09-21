151d86bee064f46b9348bfeeda5ba47d063a1a29
akashrajput326 <theakashrajput.dev@gmail.com>
2025-09-21 19:00:56 +0530

    Explored context api, react hook form, and react toastify

--- React Hook Form/src/App.jsx
+++ React Hook Form/src/App.jsx
@@ -1,8 +1,17 @@
 import SignUp from "./Components/SignUp";
 
 const App = () => {
+
+  const handleCopy = (e)=>{
+    e.clipboardData.setData('text/plain', '👆🏻');
+    e.preventDefault();
+  }
+
   return (
     <>
+    <h1
+    onCopy={handleCopy}
+    className="text-2xl">Don't copy</h1>
       <SignUp />
     </>
   );
