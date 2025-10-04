907dc94765352bf62281eb8401726307f5b73fee
akashrajput326 <theakashrajput.dev@gmail.com>
2025-10-04 17:53:59 +0530

    Wrap Recipe Website

--- Recipe Website/src/pages/Favourites.jsx
+++ Recipe Website/src/pages/Favourites.jsx
@@ -7,16 +7,18 @@ const Favourites = () => {
 
   const favouriteRecipes = recipeData.filter((ele) => ele?.favourite === true);
   return (
-    <div className="w-full min-h-screen pb-5 px-2 sm:px-4 md:px-10 md:pt-28 pt-24">
-      <h2>My Favourites ❤️</h2>
+    <div className="w-full min-h-screen px-2 sm:px-4 md:px-10 md:pt-28 pt-20">
       {favouriteRecipes.length > 0 ? (
-        <div className="w-full h-full grid grid-cols-1 grid-rows-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-5">
-          {favouriteRecipes.map((ele) => (
-            <RecipeCard key={ele.id} recipe={ele} />
-          ))}
-        </div>
+        <>
+          <h2 className="text-center mb-4 md:mb-2 text-2xl leading-none">My Favourites ❤️</h2>
+          <div className="w-full h-full grid grid-cols-1 grid-rows-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-5">
+            {favouriteRecipes.map((ele) => (
+              <RecipeCard key={ele.id} recipe={ele} />
+            ))}
+          </div>
+        </>
       ) : (
-        <div className="text-3xl">No Favourite</div>
+        <div className="w-full text-center text-3xl">No Favourite</div>
       )}
     </div>
   );
